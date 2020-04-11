be4817992b3c09c88cf591cfe56bb2336027eac8
Chhavi Kirtani <55941961+chhavikirtani2000@users.noreply.github.com>
2020-04-11 23:59:41 +0530

    Update input.rb

--- input.rb
+++ input.rb
@@ -3,3 +3,5 @@ name = gets.chomp() #gets rid of the new line character
 puts "Enter your age: "
 age = gets.chomp()
 puts ("Hello "+name + ", you are "+age)
+str = gets.chomp()
+str.gsub(/\s+/m, ' ').strip.split(" ")#splitting the input on the basis of space
